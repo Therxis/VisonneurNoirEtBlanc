47cca7599c953157ba5ec0d855051617d950fe64
Therxis <63166194+Therxis@users.noreply.github.com>
2021-11-13 13:21:18 +0100

    Ajout des fichiers de façon manuelle

---
+++ src/main/java/ihm/MainLauncher.java
@@ -0,0 +1,14 @@
+package ihm;
+
+import javafx.application.Application;
+/**
+ * Launch the Convertissor
+ * @author mathe
+ *
+ */
+public class MainLauncher {
+	public static void main(final String[] args) {
+        Application.launch(Window.class, args);
+    }
+}
+ 
